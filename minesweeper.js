5ffc76b9d1333c5702bcc2236417774b43fd0299
William Mora <wilmor24@gmail.com>
2016-04-10 18:31:29 -0300

    grid creation

--- minesweeper.js
+++ minesweeper.js
@@ -1,9 +1,14 @@
 "use strict";
 
+var _ = require("underscore");
+var collectionUtils = require("./collection_utils.js");
+
 var createGame = function(width, height, mines) {
 	validateWidth(width);
 	validateHeight(height);	
 	validateMines(mines, width, height);
+
+	return createGrid(width, height, mines);
 }
 
 function validateWidth(width) {
@@ -31,6 +36,68 @@ function validateMines(mines, width, height) {
 	if (typeof mines === "undefined" || mines < MIN_MINES || mines > MAX_MINES) {
 		throw new RangeError(`mines must be between ${MIN_MINES} and ${MAX_MINES}`);
 	}
+}
+
+function createGrid(width, height, mines) {
+	var squares = createSquaresList(width, height, mines);
+
+	var grid = collectionUtils.listToGrid(squares, width, height);
+
+	for (var i = 0; i < grid.length; i++) {
+		for (var j = 0; j < grid[i].length; j++) {
+			var square = grid[i][j];
+			if (typeof square === "undefined") {
+				var minesAround = 0;
+				var squaresToCheckForMines = [];
+				
+				if (i > 0) {
+					squaresToCheckForMines.push(grid[i - 1][j]);
+					if (j > 0) {
+						squaresToCheckForMines.push(grid[i - 1][j - 1]);
+					}
+					if (j < grid[i].length - 1) {
+						squaresToCheckForMines.push(grid[i - 1][j + 1]);
+					}
+				}
+
+				if (j > 0) {
+					squaresToCheckForMines.push(grid[i][j - 1]);
+				}
+
+				if (j < grid[i].length - 1) {
+					squaresToCheckForMines.push(grid[i][j + 1]);
+				}
+
+				if (i < grid.length - 1) {
+					squaresToCheckForMines.push(grid[i + 1][j]);
+					if (j > 0) {
+						squaresToCheckForMines.push(grid[i + 1][j - 1]);
+					}
+					if (j < grid[i].length - 1) {
+						squaresToCheckForMines.push(grid[i + 1][j + 1]);
+					}
+				}
+
+				_.each(squaresToCheckForMines, function(it) { if (typeof it !== "undefined" && it.mined === true) { minesAround++;} }); 
+				
+				grid[i][j] = { mined: false, mines_around: minesAround};
+			}
+		}
+	}
+
+	return grid;
+}
+
+function createSquaresList(width, height, mines) {
+	var squares = new Array(width * height);
+	
+	for (var i = 0; i < mines; i++) {
+		squares[i] = { mined: true };
+	}
+
+	squares = _.shuffle(squares);
+
+	return squares;
 }
 
 exports.createGame = createGame;
